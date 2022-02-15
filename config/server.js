66c7e68f453ef0b70c13258e027b0e506c58cabf
Jiang, Lin <linj2@illinois.edu>
2022-02-15 15:10:58 -0600

    add featureProjects, hero, profile and hobby

--- config/server.js
+++ config/server.js
@@ -1,7 +1,16 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1338),
   app: {
     keys: env.array('APP_KEYS'),
   },
 });
+
+// module.exports = ({ env }) => ({
+//   host: '127.0.0.1',
+//   port: 1338,
+//   url: 'https://strapi.local/api/',
+//   admin: {
+//     url: 'https://strapi.local/dashboard/',
+//   },
+// });
--- issues.md
+++ issues.md
@@ -1,1 +1,7 @@
 https://strapi.io/blog/deploying-a-strapi-api-on-heroku
+
+https://docs.strapi.io/developer-docs/latest/setup-deployment-guides/deployment/hosting-guides/heroku.html#_6-install-the-pg-node-module
+
+https://stackoverflow.com/questions/63992130/how-to-change-strapi-port-number-1337-to-other-number
+
+currently hard to implement relation in strapi
